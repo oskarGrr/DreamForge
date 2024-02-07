73aabc691170afe8102fe8ae49b3faa55c040d66
oskar <oskar@Ubuntu.myguest.virtualbox.org>
2024-02-07 04:02:16 -0600

    added echo messages to genproj.sh

--- generateProject.sh
+++ generateProject.sh
@@ -1,1 +1,3 @@
+ehco "cmake is looking for g++12. My linux distro only came with 10 or 11 by default," 
+echo " so make sure to install g++12 to use c++23 features." 
 cmake -S src -B build -DCMAKE_CXX_COMPILER=/usr/bin/g++-12
